ccfb139931bdfc87119dc7457d87c4e729a34cde
Max <maxlin2905@gmail.com>
2022-05-20 12:07:50 +1000

    2205221207

--- ngApp/src/app/users/checkout/checkout.component.ts
+++ ngApp/src/app/users/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup,FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -14,17 +14,68 @@ export class CheckoutComponent implements OnInit {
       emailField: ['',[Validators.required,Validators.email]],
       quantityField: ['',[Validators.required]],
       termField: ['',Validators.requiredTrue],
+      items:this.fb.array(
+        [
+          this.fb.group(
+            {
+              itemId:[1],
+              itemName:['item name 1'],
+              itemDesc:['item description 1'],
+              itemDone:['',Validators.requiredTrue]
+            }
+          )
+        ]
+      )
     })
 
 
    }
 
   ngOnInit(): void {
+
+// this.checkoutForm.setValue({
+//   emailField:'maxlin2905@gmail.com',
+//   quantityField:1,
+//   termField: true
+// })
+
+this.checkoutForm.patchValue({
+  emailField:'maxlin2905@gmail.com',
+  termField: true
+})
+
+this.checkoutForm.get('emailField')?.valueChanges.subscribe(
+  data=>{
+    console.log('current email value:'+data)
+  }
+)
+
+this.checkoutForm.get('emailField')?.statusChanges.subscribe(
+  data=>{
+    console.log('current email status:'+data)
+  }
+)
+
+this.checkoutForm?.statusChanges.subscribe(
+  data=>{
+    console.log('current form status:'+data)
+  }
+)
+
+
+
   }
   onSubmit(){
-    console.log(this.checkoutForm.value)
+    console.log(this.checkoutForm.value.emailField)
+    this.onReset()
+  }
+  onReset(){
+    this.checkoutForm.reset()
   }
   get f(){
     return this.checkoutForm.controls
   }
+  get items() {
+    return this.checkoutForm.get("items") as FormArray  ;
+}
 }
