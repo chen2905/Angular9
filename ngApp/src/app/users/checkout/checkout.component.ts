c73dad468c919b17604a00fd8ff84148d46ede84
Chen Gao <maxlin2905@gmail.com>
2022-05-21 15:05:12 +1000

    220521

--- ngApp/src/app/users/checkout/checkout.component.ts
+++ ngApp/src/app/users/checkout/checkout.component.ts
@@ -1,81 +1,95 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,FormControl, Validators, FormArray } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  checkoutForm: any;
+  constructor(private fb: FormBuilder) {
 
-  checkoutForm:FormGroup
-  constructor(private fb:FormBuilder) {
-    this.checkoutForm=this.fb.group( {
-      emailField: ['',[Validators.required,Validators.email]],
-      quantityField: ['',[Validators.required]],
-      termField: ['',Validators.requiredTrue],
-      items:this.fb.array(
-        [
-          this.fb.group(
-            {
-              itemId:[1],
-              itemName:['item name 1'],
-              itemDesc:['item description 1'],
-              itemDone:['',Validators.requiredTrue]
-            }
-          )
-        ]
-      )
-    })
-
-
-   }
+  }
 
   ngOnInit(): void {
+    // this.checkoutForm.setValue({
+    //   emailField:'maxlin2905@gmail.com',
+    //   quantityField:1,
+    //   termField: true
+    // })
+    this.checkoutForm = this.fb.group({
+      emailField: ['', [Validators.required, Validators.email]],
+      quantityField: ['', [Validators.required]],
+      termField: ['', Validators.requiredTrue],
+      items: this.fb.array([
+        this.fb.group({
+          itemId: ['1'],
+          itemName: ['item name 1'],
+          itemDesc: ['item description 1'],
+          itemDone: ['', Validators.requiredTrue],
+        }),
+      ]),
+    });
+    // this.checkoutForm.patchValue({
+    //   emailField: 'maxlin2905@gmail.com',
+    //   termField: true,
+    // });
 
-// this.checkoutForm.setValue({
-//   emailField:'maxlin2905@gmail.com',
-//   quantityField:1,
-//   termField: true
-// })
+    this.display()
 
-this.checkoutForm.patchValue({
-  emailField:'maxlin2905@gmail.com',
-  termField: true
-})
-
-this.checkoutForm.get('emailField')?.valueChanges.subscribe(
-  data=>{
-    console.log('current email value:'+data)
+     }
+  onSubmit() {
+    console.log(this.checkoutForm.value.emailField);
+    this.onReset();
   }
-)
+  onReset() {
+    this.checkoutForm.reset();
+  }
+ display(){
+  console.log('Items length:' + this.checkoutForm.value.items.length);
+  console.log(
+    'Items :' + JSON.stringify(this.checkoutForm.get('items')?.value)
+  );
+  console.log(
+    'Items first :' +
+      JSON.stringify(this.checkoutForm.value.items[0].itemDesc)
+  );
 
-this.checkoutForm.get('emailField')?.statusChanges.subscribe(
-  data=>{
-    console.log('current email status:'+data)
+ }
+  onResetItems() {
+    this.checkoutForm.get('items')?.reset();
   }
-)
 
-this.checkoutForm?.statusChanges.subscribe(
-  data=>{
-    console.log('current form status:'+data)
+  get f() {
+    return this.checkoutForm.controls;
   }
-)
 
-
-
-  }
-  onSubmit(){
-    console.log(this.checkoutForm.value.emailField)
-    this.onReset()
-  }
-  onReset(){
-    this.checkoutForm.reset()
-  }
-  get f(){
-    return this.checkoutForm.controls
-  }
   get items() {
-    return this.checkoutForm.get("items") as FormArray  ;
-}
+    return this.checkoutForm.get('items') as FormArray;
+  }
+
+  addItem(){
+    const itemLength = this.items.length +1
+    const newItem =this.fb.group({
+      itemId: [itemLength],
+      itemName: ['ss'],
+      itemDesc: ['ssss'],
+      itemDone: ['', Validators.requiredTrue],
+    })
+    this.items.push(newItem);
+
+    this.display()
+  }
+  removeItemAt(index : number){
+    this.items.removeAt(index)
+  }
+
+
 }
