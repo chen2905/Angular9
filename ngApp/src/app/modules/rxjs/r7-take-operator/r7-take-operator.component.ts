95431933fdf97bcbfac269af2ce4f185e0cab94c
Chen Gao <maxlin2905@gmail.com>
2022-06-15 21:35:20 +1000

    22061501

--- ngApp/src/app/modules/rxjs/r7-take-operator/r7-take-operator.component.ts
+++ ngApp/src/app/modules/rxjs/r7-take-operator/r7-take-operator.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, take } from 'rxjs';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fromEvent, interval, Observable, take, takeLast, takeUntil, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-r7-take-operator',
   templateUrl: './r7-take-operator.component.html',
   styleUrls: ['./r7-take-operator.component.css']
 })
-export class R7TakeOperatorComponent implements OnInit {
+export class R7TakeOperatorComponent implements OnInit,AfterViewInit {
+
+  onTakeUntilEvent : Observable<Event>|undefined
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit(): void {
+      this.onTakeUntilEvent=fromEvent(document.getElementById('btnTakeUntil')!,'click')
+  }
+
   onTake(){
     interval(1000)
     .pipe(take(10))
@@ -25,4 +32,39 @@ export class R7TakeOperatorComponent implements OnInit {
 
     )
   }
+
+  onTakeLast(){
+   interval(500)
+   .pipe(take(15),takeLast(3),tap((data)=>'last 3 data'+data))
+   .subscribe((data)=>{
+    console.log(data)
+   })
+  }
+  onStartInterval(){
+    interval(500)
+    .pipe(takeUntil(this.onTakeUntilEvent!))
+    .subscribe(
+      data =>{
+        console.log(data)
+      },
+      error=>{
+        console.log(error)
+      },
+      ()=>console.log('takeuntil complete')
+    )
+  }
+
+  onTakeWhile(){
+    interval(500).pipe(
+      takeWhile((x)=>x<3)
+    ).subscribe(data=>{
+      console.log(data)
+    },
+    error=>{
+      console.log(error)
+    },
+    ()=>console.log('takeWhile complete')
+    )
+  }
+
 }
