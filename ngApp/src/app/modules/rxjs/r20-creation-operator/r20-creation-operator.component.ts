3029316b3f95c15ab45d86bc64e9e2abe56ac021
Chen Gao <maxlin2905@gmail.com>
2022-07-03 23:05:31 +1000

    220730

--- ngApp/src/app/modules/rxjs/r20-creation-operator/r20-creation-operator.component.ts
+++ ngApp/src/app/modules/rxjs/r20-creation-operator/r20-creation-operator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { defer, of, range } from 'rxjs';
+import { asyncScheduler, defer, generate, interval, of, range, timer } from 'rxjs';
+import { GenerateOptions } from 'rxjs/internal/observable/generate';
 
 @Component({
   selector: 'app-r20-creation-operator',
@@ -62,4 +63,59 @@ export class R20CreationOperatorComponent implements OnInit {
     () => (this.completeMessage2 = 'observer 2 complete')
   );
   }
+
+useGenerate(){
+
+  let generateCondition :GenerateOptions<string,number>={
+    initialState:1,
+    condition:(x)=>x<100,
+    iterate:(x)=> x+1  ,
+    resultSelector:(x)=>'Thanks God! '+x +' times',
+    scheduler:asyncScheduler
+  }
+  let source$= generate(generateCondition);
+
+  source$.subscribe(
+    (data) => {
+      this.outputSet1.push('observer 1 reciving: ' + data);
+    },
+    (err) => (this.errorMessage1 = err),
+    () => (this.completeMessage1 = 'observer 1 complete')
+  );
+
+  source$.subscribe(
+    (data) => {
+      this.outputSet2.push('observer 2 reciving: ' + data);
+    },
+    (err) => (this.errorMessage2 = err),
+    () => (this.completeMessage2 = 'observer 2 complete')
+  );
+
+}
+useTimer(){
+  let intervalSource$ = interval(2000)
+  let timerSource$ = timer(1000,2000)
+
+
+
+  intervalSource$.subscribe(
+    (data) => {
+      this.outputSet1.push('inserval emitting: ' + data);
+    },
+    (err) => (this.errorMessage1 = err),
+    () => (this.completeMessage1 = 'observer 1 complete')
+  );
+
+  timerSource$.subscribe(
+    (data) => {
+      this.outputSet2.push('timer emitting: ' + data);
+    },
+    (err) => (this.errorMessage2 = err),
+    () => (this.completeMessage2 = 'observer 2 complete')
+  );
+
+}
+
+
+
 }
